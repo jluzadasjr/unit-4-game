fd410f25d5f2f0c338cfd4de5f526b11dda0057d
jluzadasjr <jayrl707@unknowndca9048e9fd1.attlocal.net>
2019-01-10 23:33:02 -0800

    updated code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,64 +3,110 @@
 //Each crystal needs a random number from 1-12
 //A new random number should generate every single time we win or lose 
 //When clicking a random crystal, add from previous score until it hits the target score
-//if it is greather than the random result, decrement loss count
-//if it is equal, we increment win count
-//if it does not equal targeted score, then game resets
-
-
-// Set up Global variables
-var randScore = 0;
-var winCount;
-var lossCount;
-
+//If it is greather than the random result, decrement loss count
+//If it is equal, we increment win count
+//If it does not equal targeted score, then game resets
 
 $(document).ready(function (){
+    var randTarget = Math.floor(Math.random() * 101) + 19;
+    // console.log(randTarget);
 
+    $("#scoretarget").html("Target Total: " + randTarget);
 
- var randTarget = Math.floor(Math.random() * 101) + 19;
+    var crystal1= Math.floor(Math.random() * 11) + 1;
+    var crystal2= Math.floor(Math.random() * 11) + 1;
+    var crystal3= Math.floor(Math.random() * 11) + 1;
+    var crystal4= Math.floor(Math.random() * 11) + 1;
 
-//  console.log(randTarget); 
+// Set up global variables
 
-$(".score").html("Target Score: " + randTarget);
+    var yourTotal;
+    var winCount = 0;
+    var lossCount = 0;
 
-var yourScore = Math.floor(Math.random() * 11) + 1;
+    $("#record").html("Wins: " + "<br>" + "<br>" + "Losses: ");
 
-$(".totalscore").html("Your score: " + yourScore);
+// Function to restart the game
+    function restart () {
+        random = Math.floor(Math.random() * 102 + 19)
+        // console.log(random)
+        $("#scoretarget").html(random); 
+        crystal1 = Math.floor(Math.random() * 11) + 1;
+        crystal2 = Math.floor(Math.random() * 11) + 1;
+        crystal3 = Math.floor(Math.random() * 11) + 1;
+        crystal4 = Math.floor(Math.random() * 11) + 1;
+        yourTotal = 0;
+        $("#scoretarget").html(yourTotal);
+    }
 
-// console.log(numOptions)
+//Show that the player wins
+function winner () {
+    winCount++;
+    $("#record").html("Win: " + winCount);
+    restart();
+    };
+   
+//Show that the player loses
+function loser () {
+    lossCount++;
+    $("#record").html(lossCount);
+    restart();
+    };
 
+//Create the on-click events for the crystal buttons
+$(".crystal1").on('click', function() {
+    yourTotal = yourTotal + crystal1;
+    $("#record").html("Wins: " + winCount + "<br>" + "<br>" + "Losses: " + lossCount);
 
-for (i = 0; i < 4; i++) {
-    var imageCrystal = $("#crystal");
-    imageCrystal.addClass("data-random-value");
-    $("#crystal").append(imageCrystal);
-
-}
-
-
-$("#crystal").on('click', function() {
-    var result = ($(this).attr("data-random-value"));
-    result = parseInt(result);
-    yourScore += result;
-    console.log(result);
-
-if (yourScore === randTarget) {
-    $(".totalscore").html("Win: " + winCount++);
-
-}
-
-else if (yourScore >= randTarget) {
-    $(".totalscore").html("You lose: " + lossCount++)
-    $(".totalscore").empty();
-    // console.log(alert)
-}
+//conditional statements
+    if (yourTotal == randTarget) {
+        winner();
+    }
+    else if (yourTotal > randTarget) {
+        loser();
+    }
 
 });
 
+$(".crystal2").on('click', function() {
+    yourTotal = yourTotal + crystal2;
+    $("#record").html("Wins: " + winCount + "<br>" + "<br>" + "Losses: " + lossCount);
+//conditional statements
+    if (yourTotal == randTarget) {
+        winner();
+    }
+
+    else if (yourTotal > randTarget) {
+        loser();
+    }
 });
 
+$(".crystal3").on('click', function() {
+    yourTotal = yourTotal + crystal3;
+    $("#record").html("Wins: " + winCount + "<br>" + "<br>" + "Losses: " + lossCount);
+//conditional statements
+    if (yourTotal == randTarget) {
+        winner();
+    }
 
+    else if (yourTotal > randTarget) {
+        loser();
+    }
+});
 
+$(".crystal4").on('click', function() {
+    yourTotal = yourTotal + crystal4;
+    $("#record").html("Wins: " + winCount + "<br>" + "<br>" + "Losses: " + lossCount);
+//conditional statements
+});
+    if (yourTotal == randTarget) {
+        winner();
+    }
+
+    else if (yourTotal > randTarget) {
+        loser();
+    }
+})
 
 
 
